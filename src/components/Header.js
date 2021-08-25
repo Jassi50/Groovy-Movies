1a79dd2c798f7e05885427a3a79a095523d735b7
jmaguss <magussjohn@gmail.com>
2021-08-24 20:48:18 -0600

    Header/beta.01
    Here's the header.... There is an issue with the nav/ul .menu.... the drop downs work for mobile, but for desktop the links no longer work?? Also currently there is a bug where you can view multiple dropdown menus at the same time

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,80 @@
-import { NavLink } from 'react-router-dom';
-import NavMain from './NavMain'
+/** @format */
+
+import { NavLink } from "react-router-dom";
+import NavMain from "./NavMain";
+import { FaSearch } from "react-icons/fa";
+import { useState, useEffect } from "react";
+import { FaFilter } from "react-icons/fa";
+import logo from "../images/logo.svg";
 
 function Header() {
+    const [toggleSearch, setToggleSearch] = useState(false);
+    const [toggleFilter, seToggleFilter] = useState(false);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    const searchToggle = () => {
+        setToggleSearch(!toggleSearch);
+    };
+    const filterToggle = () => {
+        seToggleFilter(!toggleFilter);
+    };
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        };
+        window.addEventListener("resize", changeWidth);
+    }, []);
+
     return (
         <header>
             {/*This will later become an image to reflect my logo */}
-            <h1><NavLink to="/">Groovy Movies</NavLink></h1> 
+            <h1>
+                <NavLink to="/">
+                    <img className="logo" src={logo} alt="logo" />
+                </NavLink>
+            </h1>
+
+            <FaSearch onClick={searchToggle} className="search-icon" />
+
+            <label onClick={filterToggle} className="btn-filter" for="toggle">
+                <FaFilter onClick={filterToggle} className="filter-icon" />
+            </label>
+
             <NavMain />
 
-            <div className="search-bar">
-				<input type="text" placeholder="Search.." />
-			</div>
+            {(toggleSearch || screenWidth > 800) && (
+                <div className="search-bar">
+                    <input type="text" placeholder="Search.." />
+                </div>
+            )}
+
+            {toggleFilter && (
+                <nav className="nav-sort1">
+                    <ul>
+                        <li>
+                            <NavLink to="/sort/popular">Popular</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/sort/top-rated">Top Rated</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/sort/in-theatres">
+                                In Theatres
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/sort/comming-soon">
+                                Coming Soon
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+
+            {/* <FaSearch className="search-icon-mobile" /> */}
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
--- src/components/NavMain.js
+++ src/components/NavMain.js
@@ -1,14 +1,45 @@
 import { NavLink } from 'react-router-dom';
+import {FaBars} from 'react-icons/fa';
+import {useState, useEffect} from 'react';
 
 function NavMain() {
+
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] =useState(window.innerWidth)
+    
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
+    useEffect(()=> {
+
+         const changeWidth = () => {
+             setScreenWidth(window.innerWidth)
+            }
+            window.addEventListener('resize', changeWidth)
+    }, [])
+
+
+
     return (
-        <nav>
-          <ul>
+        <>
+          <label onClick={toggleNav} className="btn" for="toggle"><FaBars/></label>
+        <nav className="nav">
+            {(toggleMenu || screenWidth > 800) && ( <ul className="menu">
               <li><NavLink to="/" exact>Home</NavLink></li>
               <li><NavLink to="/about">About</NavLink></li>
               <li><NavLink to="/favorites">Favorites</NavLink></li>
-          </ul>  
+          </ul>  )}
+            
+           
+         
+        
+           
         </nav>
+      
+
+        </>
     )
 }
 
