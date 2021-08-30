ed5d2e67749d39fdc012764622678db47a64f31f
jmaguss <magussjohn@gmail.com>
2021-08-29 21:22:14 -0600

    updated header
    Updated header.... Nav menu links work both on mobile and desktop, however clicking on the text does not work... you have to click either above or below for it to work... still trying to figure out why this is.. nav menu's still overlap...

--- src/components/NavMain.js
+++ src/components/NavMain.js
@@ -1,6 +1,9 @@
 import { NavLink } from 'react-router-dom';
 import {FaBars} from 'react-icons/fa';
 import {useState, useEffect} from 'react';
+import hover from '../images/LightHover.png'
+
+
 
 function NavMain() {
 
@@ -23,13 +26,16 @@ function NavMain() {
 
 
     return (
-        <>
+        <div className="nav-container">
+           
           <label onClick={toggleNav} className="btn" for="toggle"><FaBars/></label>
         <nav className="nav">
             {(toggleMenu || screenWidth > 800) && ( <ul className="menu">
-              <li><NavLink to="/" exact>Home</NavLink></li>
-              <li><NavLink to="/about">About</NavLink></li>
-              <li><NavLink to="/favorites">Favorites</NavLink></li>
+              <li><NavLink to="/" exact>
+              <img className="hover" src={hover} alt="hover-effect"/>
+                  Home</NavLink></li>
+              <li><NavLink to="/about"> <img className="hover" src={hover} alt="hover-effect"/>About</NavLink></li>
+              <li><NavLink to="/favorites" ><img className="hover favorites" src={hover} alt="hover-effect"/>Favorites</NavLink></li>
           </ul>  )}
             
            
@@ -39,7 +45,7 @@ function NavMain() {
         </nav>
       
 
-        </>
+        </div>
     )
 }
 
