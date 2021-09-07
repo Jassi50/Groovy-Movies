f8d8fd6f450c5310fc89f7a067e6cea3a0d134ff
jmaguss <magussjohn@gmail.com>
2021-09-06 23:14:09 -0600

    About + Footer
    Here's the about + footer section

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,10 +1,13 @@
+import '../styles/_about.scss'
 function PageAbout() {
     return (
         <section className="about-page">
-            <h2>About us</h2>
-            <p>
+            <div className="about-container">
+            <h2 className="about-header">About us</h2>
+            <p >
                 Welcome to Groovy Movies! We are a growing movie database website, where you can view reviews on movies and share your own oppinions with our community! So don’t hesitate to kick back and watch some groovy movies.
             </p>
+            </div>
         </section>
     )
 }
